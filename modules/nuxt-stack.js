3b417bdc84e71f122b6ea1c3aaf0a4559f3a7ff1
David Royer <droyer01@gmail.com>
2019-12-12 01:26:52 -0500

    chore: test

--- modules/nuxt-stack.js
+++ modules/nuxt-stack.js
@@ -1,16 +1,8 @@
 export default function(moduleOptions) {
-  // eslint-disable-next-line no-console
-  console.log('HELLO')
-
   this.extendBuild((config, { isClient, isServer }) => {
     config.module.rules.push({
       test: /\.ya?ml$/,
       use: 'js-yaml-loader'
     })
   })
-
-  if (process.client) {
-    // eslint-disable-next-line no-console
-    console.log('FROM MODULE')
-  }
 }
--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,40 @@
 <template>
-  <v-row align="center">
+  <v-row align="center" justify="center">
     <v-col cols="12" md="5">
-      <h2>Login Here</h2>
-      <v-btn @click="handleGoogleLogin" color="primary">Google Login</v-btn>
+      <v-card width="500" class="mx-auto mt-5">
+        <v-card-title class="pb-0">
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              label="Email"
+              type="email"
+              prepend-icon="mdi-account-circle"
+            />
+            <v-text-field
+              :type="showPassword ? 'text' : 'password'"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="showPassword = !showPassword"
+              label="Password"
+              prepend-icon="mdi-lock"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn light>
+            <v-icon>mdi-email</v-icon>
+            Email Signup
+          </v-btn>
+          <v-spacer />
+          <v-btn color="primary lighten-1">
+            Google Login
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+      <!-- <h2>Login Here</h2>
+      <v-btn @click="handleGoogleLogin" color="primary">Google Login</v-btn> -->
     </v-col>
   </v-row>
 </template>
@@ -10,6 +42,10 @@
 <script>
 export default {
   middleware: 'login-route-guard',
+
+  data: () => ({
+    showPassword: false
+  }),
 
   methods: {
     async handleGoogleLogin() {
